ddf1f17853d80a0786ab3cd37f77dffbf6fec87b
Vegemash <mrisaacjackson@gmail.com>
2017-08-17 14:14:07 +1000

    Set a bunch of stuff as inactive

--- install/oe-cera-clean.sh
+++ install/oe-cera-clean.sh
@@ -31,13 +31,19 @@ BEGIN
 
   INSERT INTO openeyes.site
   (name, remote_id, short_name, location_code, fax, telephone, last_modified_user_id, last_modified_date, created_user_id, created_date, institution_id, location, contact_id, replyto_contact_id, source_id, active)
-  VALUES ('Center for Eye Research Australia', 'CERA', 'CERA', '', '', '0399298360', 1, NOW(), 1, NOW(), institution_id, '', contact_id, null, null, 1);
+  VALUES ('Center for Eye Research Australia', 'CERA', 'CERA', '', '', '0399298360', 1, NOW(), 1, NOW(), institution_id, '', contact_id, null, import_source_id, 1);
 END;
 CALL make_cera;
 DROP PROCEDURE IF EXISTS make_cera;
 
+SET FOREIGN_KEY_CHECKS = 0;
+DELETE FROM user WHERE id != 1;
+DELETE FROM patient;
+SET FOREIGN_KEY_CHECKS = 1;
+
+UPDATE firm SET active = FALSE WHERE id !=1 AND consultant_id IS NOT NULL;
 UPDATE institution SET active = FALSE WHERE LOWER(name) NOT LIKE '%center for eye research%';
-UPDATE site SET active = TRUE WHERE remote_id != 'CERA';
+UPDATE site SET active = FALSE WHERE remote_id != 'CERA';
 ;;
 " > /tmp/openeyes-mysql-cera-setup.sql
 
